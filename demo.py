575d8664acdb6bde0fa169763e050325882348e2
Ashis Das <105511844+ashis-das@users.noreply.github.com>
2022-11-13 00:10:13 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,5 @@
+# Addition of two numbers
+#take two user input number and add them
+num1=int(input("enter 1st integer no."))
+num2=int(input("enter 2nd integer no."))
+print("the addition of",num1 ,"and",num2 ,"are",num1+num2)
